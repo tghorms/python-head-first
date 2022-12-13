e08054e05232ba06c25214b01f7edaa73e09bb5d
tghorms <ghormleythomas@yahoo.com>
2022-12-12 22:34:48 -0600

    feat: Dog Age Calculator

---
+++ chapter-one/DogAgeCalc.py
@@ -0,0 +1,20 @@
+dog_name = input("What is your dog's name? ")
+
+dog_age = input("What is the dog's age? ")
+
+human_years = int(dog_age) * 7
+
+print('Your dog', dog_name, 'is', human_years, 'years old.')
+
+
+# first = 'somewhere'
+
+# last = 'over the rainbow'
+
+# print(first, last)
+
+# first = 'over the rainbow'
+
+# last = 'somewhere'
+
+# print(first, last)
