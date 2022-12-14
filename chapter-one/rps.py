f7aa6ab5a5a8cec4a63803ae23c10718b3960138
tghorms <ghormleythomas@yahoo.com>
2022-12-14 00:19:59 -0600

    feat: need a loop for invali resopnse

--- chapter-one/rps.py
+++ chapter-one/rps.py
@@ -5,6 +5,8 @@ selections = ['rock', 'paper', 'scissors']
 user_input = input("rock, paper, scissors? ")
 comp_choice = random.choice(selections)
 print('The computer choses ' + comp_choice)
+if user_input != 'rock' or user_input != 'paper' or user_input != 'scissors':
+    print('Invaild response')
 
 if user_input == 'rock' and comp_choice == 'rock': 
     print('User tied :(')
